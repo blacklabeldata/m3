2fc8f1d6284dec070e16ad40577e9e0e7d9677cd
eliquious <max.franks@synapse-wireless.com>
2015-05-16 20:55:36 -0500

    Improve test coverage on core

--- core_test.go
+++ core_test.go
@@ -41,6 +41,54 @@ func (c *CountingWriteCloser) Close() error {
     return nil
 }
 
+func NewRandReader(table *crc64.Table) *RandReadCloser {
+    return &RandReadCloser{false, 0, 0, table}
+}
+
+type RandReadCloser struct {
+    Closed bool
+    CRC64  uint64
+    Count  int
+    table  *crc64.Table
+}
+
+func (r *RandReadCloser) Read(data []byte) (n int, err error) {
+    r.Count += len(data)
+    n, err = rand.Read(data)
+
+    r.CRC64 = crc64.Update(r.CRC64, r.table, data)
+    return
+}
+
+func (r *RandReadCloser) Close() error {
+    r.Closed = true
+    return nil
+}
+
+func NewStatsReader(table *crc64.Table, r io.ReadCloser) *StatsReadCloser {
+    return &StatsReadCloser{false, 0, 0, table, r}
+}
+
+type StatsReadCloser struct {
+    Closed bool
+    CRC64  uint64
+    Count  int
+    table  *crc64.Table
+    parent io.ReadCloser
+}
+
+func (r *StatsReadCloser) Read(data []byte) (n int, err error) {
+    r.Count += len(data)
+    n, err = r.parent.Read(data)
+    r.CRC64 = crc64.Update(r.CRC64, r.table, data)
+    return
+}
+
+func (r *StatsReadCloser) Close() error {
+    r.Closed = true
+    return r.parent.Close()
+}
+
 func TestNewWriter(t *testing.T) {
 
     // create test data
@@ -103,4 +151,68 @@ func TestWriterUse(t *testing.T) {
     assert.Nil(t, err)
     assert.True(t, crcWriter.Closed)
     assert.True(t, countingWriter.Closed)
+}
+
+func TestNewReader(t *testing.T) {
+
+    // create test data
+    buffer := make([]byte, 1024)
+
+    // test utils
+    table := crc64.MakeTable(crc64.ISO)
+    randReader := NewRandReader(table)
+
+    // create reader
+    reader := NewReader(randReader)
+
+    // test read
+    n, err := reader.Read(buffer)
+    assert.Nil(t, err)
+    assert.Equal(t, n, len(buffer))
+    assert.Equal(t, n, randReader.Count)
+
+    // calc chesksum
+    csum := crc64.Checksum(buffer, table)
+    assert.Equal(t, randReader.CRC64, csum)
+
+    // test close
+    err = reader.Close()
+    assert.Nil(t, err)
+    assert.True(t, randReader.Closed)
+}
+
+func TestNewReaderUse(t *testing.T) {
+
+    // create test data
+    buffer := make([]byte, 1024)
+
+    // test utils
+    table := crc64.MakeTable(crc64.ISO)
+    randReader := NewRandReader(table)
+    var statsReader *StatsReadCloser
+
+    // create reader
+    reader := NewReader(randReader)
+    reader.Use(func(r io.ReadCloser) io.ReadCloser {
+        statsReader = NewStatsReader(table, r)
+        return statsReader
+    })
+
+    // test read
+    n, err := reader.Read(buffer)
+    assert.Nil(t, err)
+    assert.Equal(t, n, len(buffer))
+    assert.Equal(t, n, randReader.Count)
+    assert.Equal(t, n, statsReader.Count)
+
+    // calc chesksum
+    csum := crc64.Checksum(buffer, table)
+    assert.Equal(t, randReader.CRC64, csum)
+    assert.Equal(t, statsReader.CRC64, csum)
+
+    // test close
+    err = reader.Close()
+    assert.Nil(t, err)
+    assert.True(t, randReader.Closed)
+    assert.True(t, statsReader.Closed)
 }
